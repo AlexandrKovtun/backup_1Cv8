0b252b86d125d121faa9f701e4bc4ed7020668d3
AlexandrKovtun <51018441+AlexandrKovtun@users.noreply.github.com>
2019-06-01 19:55:15 +0500

    Update backup_1Cv8.py
    Внесены небольшие доработки.
    Добавил возможность вводить префикс архива базы данных.

--- backup_1Cv8.py
+++ backup_1Cv8.py
@@ -1,17 +1,18 @@
-#-*- coding: utf-8 -*-
+
 from datetime import datetime
-from os import remove, system
+from os import remove, system, path
 import zipfile
 from clint.textui import colored # модуль для печати разными цветами
-from pathlib import Path
+from time import gmtime, strftime
 
-system("mode con cols=65 lines=15") # задаём размер окна консоли
+system("mode con cols=65 lines=17") # задаём размер окна консоли
 
-print(" =============================================================== ")
+print("")
 print("           РЕЗЕРВНОЕ КОПИРОВАНИЕ ФАЙЛА БАЗЫ 1Cv8.1CD")
 print(" =============================================================== ")
 
 today = datetime.today() # получаем текущую дату
+
 # конвертирует байты в Кб, Мб, Гб и Тб
 def convert_bytes(num):
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
@@ -25,11 +26,15 @@ try: 										# проверяем, есть ли файл базы в папк
     file1 = open(filename1,"rb")
 except IOError: 							# если нет, то выкинуть исключение
     print (colored.red ("\n ОШИБКА!") )
-    print(" Файла базы (1Cv8.1CD) нет в этой папке!")
-    print(" Поместите программу в папку с базой...")
+    print(" Файла базы (1Cv8.1CD) нет в этой папке!\n Поместите программу в папку с базой...")
 else:
-	start_time = datetime.today() # начинаем отсчёт времени
-	filename2 = "backup_" + today.strftime("%d.%m.%Y_%H.%M") + "_" + filename1
+	start_time = datetime.today() # начинаем отсчёт времени today.strftime("%d.%m.%Y_%H.%M", gmtime())
+	#filename2 = "backup_" + today.strftime("%d.%m.%Y_%H.%M") + "_" + filename1
+	filename2 = input(' Введите префикс бэкапа или нажмите "ENTER":\n ')
+	if filename2 == "":
+		filename2 = "backup_" + today.strftime("%d.%m.%Y_%H.%M") + "_" + filename1
+	else:
+		filename2 = filename2 + "_" + today.strftime("%d.%m.%Y_%H.%M") + "_" + filename1	
 	file2 = open(filename2,"wb")
 	print(" Копирование начато! Ожидайте...")
 	file2.write(file1.read())				# копирование
@@ -44,16 +49,15 @@ else:
 	my_zip = zipfile.ZipFile(zip_name, 'w')
 	my_zip.write(filename2, compress_type=zipfile.ZIP_DEFLATED)
 	my_zip.close()
+
 	remove(filename2) # удаляем созданную копию файла
 
-	# Получаем размер архива
-	file = Path(zip_name)
-	size = file.stat().st_size
+	size = path.getsize(zip_name) # получаем размер файла
 
     # Создаём лог-файл backup_log.txt
 	log_filename = "backup_1C.log"
 	log = open(log_filename, "a")
-	log.write(today.strftime("%d.%m.%Y_%H.%M") + " Создан архив " + zip_name + "\n                 Размер архива: " + convert_bytes(size)+ ". Время выполнения: " + str(datetime.today() - start_time) + ".\n")
+	log.write(today.strftime("%d.%m.%Y_%H.%M") + " Создан архив " + zip_name + "\n                 Размер архива: "+ convert_bytes(size) + ". Время выполнения: " + str(datetime.today() - start_time) + ".\n")
 	log.close()
 
 	# Сообщения, при успешном выполнении
